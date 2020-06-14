aa64aeb80fb42f343c8bca3228b4eee17a151d27
deveshbamola <deveshbamola@gmail.com>
2020-06-14 14:48:19 +0530

    files

---
+++ project_files/Assets/scripts/scene_loader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_loader : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    public void scene1()
+    {
+        SceneManager.LoadScene("Scene!");
+    }
+    public void scene2()
+    {
+        SceneManager.LoadScene("secen2");
+    }
+    public void scene3()
+    {
+        SceneManager.LoadScene("scene3");
+    }
+}
